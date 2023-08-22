c40a06339151f37d0f75208ccc8327f0fc83d48b
tone <llanoajm@gmail.com>
2023-08-21 20:17:11 -0500

    Weight strand significance according to the statistical impact of a single domain

--- project/graphics.py
+++ project/graphics.py
@@ -51,4 +51,30 @@ for strand_sequence in obtained_strands:
     strand_details = strands_lookup.get(strand_sequence)
     if strand_details:
         matched_strands.append(strand_details)
-        
+
+
+def draw_complex(matched_strands):
+    fig, ax = plt.subplots()
+    ax.axis('off')
+    colors = ['blue', 'red', 'green', 'purple']  # Add more colors if needed
+
+    # Starting positions
+    x_top, y_top = 1, 0.6
+    x_bottom, y_bottom = 0, 0.3
+
+    for idx, strand_details in enumerate(matched_strands):
+        strand = strand_details['sequence']
+        orientation = 'right-to-left' if idx % 2 == 0 else 'left-to-right'
+        floor = 'top' if idx % 4 < 2 else 'bottom'
+
+        if floor == 'top':
+            ax.text(x_top, y_top, strand[::-1] if orientation == 'right-to-left' else strand, fontsize=12, color=colors[idx])
+            x_top -= 0.3  # Adjust as needed
+        else:
+            ax.text(x_bottom, y_bottom, strand[::-1] if orientation == 'right-to-left' else strand, fontsize=12, color=colors[idx])
+            x_bottom += 0.3  # Adjust as needed
+
+    plt.show()
+
+# Example usage
+draw_complex(matched_strands)

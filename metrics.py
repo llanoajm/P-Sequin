16c6ed067e842a846fabb466e42680c3d67e879b
tone <llanoajm@gmail.com>
2023-09-27 23:01:52 -0500

    Try new nupack

--- metrics.py
+++ metrics.py
@@ -1,26 +1,28 @@
 # all metrics are to be minimized
 
 import random
-# import nupack as nu # installed on /usr/local/bin/python3, not on conda                     
+import sys
+sys.path.append('NUPACK/src/source')
 
 
 def compute_stability(sequence):
-    
-    # my_model = nu.Model(material='DNA')
-    # result = nu.pfunc([sequence], model=my_model)
-    # # Return the negative logarithm of the partition function as a measure of stability
-    # return float(-result[0]) #.log_pfunc
-    return 0
+    #lazy import bc there is a circular
+
+    my_model = nu.Model(material='DNA')
+    result = nu.pfunc([sequence], model=my_model)
+    # Return the negative logarithm of the partition function as a measure of stability
+    return float(-result[0]) #.log_pfunc
+   
 
 
 def check_secondary_structures(sequence):
-    
-    # my_model = nu.Model(material='DNA')
-    # result = nu.mfe([sequence], model=my_model)
-    # # Return the free energy of the MFE structure
-    # # Lower values (more negative) indicate more stable secondary structures
-    # return result[0].energy
-    return 0
+
+    my_model = nu.Model(material='DNA')
+    result = nu.mfe([sequence], model=my_model)
+    # Return the free energy of the MFE structure
+    # Lower values (more negative) indicate more stable secondary structures
+    return result[0].energy
+
 
 
 def check_if_palindrome(sequence):
